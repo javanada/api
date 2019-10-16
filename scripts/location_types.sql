005a7564a7c7e5f5f3c237bb733ad8783ce1c084
David Sergio <davidsergio52@gmail.com>
2019-10-16 14:58:08 -0700

    location types

--- scripts/location_types.sql
+++ scripts/location_types.sql
@@ -10,8 +10,8 @@ START TRANSACTION;
 -- Table structure for table `objects`
 --
 
-CREATE TABLE IF NOT EXISTS `i_nav`.`object_types` (
-  `object_type_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `i_nav`.`location_types` (
+  `location_type_id` int(11) NOT NULL,
   `short_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   `long_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS `i_nav`.`object_types` (
 -- Indexes for table `objects`
 --
 
-ALTER TABLE `object_types`
-  ADD FOREIGN KEY (`object_type_id`) REFERENCES `objects`(`object_type_id`);
+ALTER TABLE `location_types`
+  ADD FOREIGN KEY (`location_type_id`) REFERENCES `locations`(`location_type_id`);
 
 COMMIT;
 
