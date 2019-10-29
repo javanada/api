9add327565f26680690ed23ada9ee0a429d056d4
David Sergio <davidsergio52@gmail.com>
2019-10-28 21:30:37 -0700

    updates

--- scripts/insert_all.sql
+++ scripts/insert_all.sql
@@ -15,7 +15,7 @@ INSERT INTO `object_types` (`short_name`, `long_name`, `description`)
 VALUES ('path', NULL, NULL);
 
 INSERT INTO `objects` (`location_id`, `short_name`, `long_name`, `description`, `object_type_id`, `x_coordinate`, `y_coordinate`, `latitude`, `longitude`, `active`) 
-VALUES (NULL, 'object1', NULL, NULL, 1, 0, 0, NULL, NULL, 1);
+VALUES (1, 'object1', NULL, NULL, 1, 0, 0, NULL, NULL, 1);
 
 INSERT INTO `locations` (`short_name`, `long_name`, `description`, `location_type_id`, `address_id`, `primary_object_id`, `scale_ft`, `min_x_coordinate`, `min_y_coordinate`, `max_x_coordinate`, `max_y_coordinate`, `latitude`, `longitude`, `active`) 
 VALUES ('ceb101', NULL, 'the cave', 1, 1, 1, 1, '0', '0', '100', '100', NULL, NULL, 1);
