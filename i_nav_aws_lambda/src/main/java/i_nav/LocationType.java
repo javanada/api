0e097c44af90e5c55e91d86b639a3c719ae720e8
David Sergio <davidsergio52@gmail.com>
2019-10-31 18:25:31 -0700

    updates

--- i_nav_aws_lambda/src/main/java/i_nav/LocationType.java
+++ i_nav_aws_lambda/src/main/java/i_nav/LocationType.java
@@ -2,6 +2,11 @@ package i_nav;
 
 public class LocationType implements INavEntity {
 	
+	private int location_type_id;
+	private String short_name;
+	private String long_name;
+	private String description;
+	
 	public static String getLocationTypes(String id) {
 		return null;
 	}
@@ -11,5 +16,38 @@ public class LocationType implements INavEntity {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	public int getLocation_type_id() {
+		return location_type_id;
+	}
+
+	public void setLocation_type_id(int location_type_id) {
+		this.location_type_id = location_type_id;
+	}
+
+	public String getShort_name() {
+		return short_name;
+	}
+
+	public void setShort_name(String short_name) {
+		this.short_name = short_name;
+	}
+
+	public String getLong_name() {
+		return long_name;
+	}
+
+	public void setLong_name(String long_name) {
+		this.long_name = long_name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 	
 }
