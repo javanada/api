9045c721e2980e48cc5acd759cb7d96051d1743d
David Sergio <davidsergio52@gmail.com>
2019-11-01 16:57:35 -0700

    added object types

---
+++ i_nav_aws_lambda/src/main/java/i_nav/LocationObjectTypes.java
@@ -0,0 +1,11 @@
+package i_nav;
+
+public class LocationObjectTypes implements INavEntity {
+
+	@Override
+	public String getJSONString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
