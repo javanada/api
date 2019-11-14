a3df3196ff87dfbc259eafeebeb78354c09cd697
David Sergio <davidsergio52@gmail.com>
2019-11-14 00:08:21 -0800

    updated locations

--- scripts/locations.sql
+++ scripts/locations.sql
@@ -18,14 +18,6 @@ CREATE TABLE IF NOT EXISTS `i_nav`.`locations` (
   `image` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
   `location_type_id` int(11) NOT NULL,
   `address_id` int(11) NOT NULL,
-  `primary_object_id` int(11) DEFAULT NULL,
-  `scale_ft` float(10) NOT NULL,
-  `min_x_coordinate` int(8) NOT NULL,
-  `min_y_coordinate` int(8) NOT NULL,
-  `max_x_coordinate` int(8) NOT NULL,
-  `max_y_coordinate` int(8) NOT NULL,
-  `latitude` decimal(9,6) DEFAULT NULL,
-  `longitude` decimal(9,6) DEFAULT NULL,
   `active` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -38,8 +30,6 @@ ALTER TABLE `locations`
   ADD FOREIGN KEY (`location_type_id`) REFERENCES `location_types`(`location_type_id`);
 ALTER TABLE `locations`
   ADD FOREIGN KEY (`address_id`) REFERENCES `addresses`(`address_id`);
-ALTER TABLE `locations`
-  ADD FOREIGN KEY (`primary_object_id`) REFERENCES `objects`(`object_id`);
 
 -- AUTO_INCREMENT for table `locations`
 --
